8bfaaf71efd33ea761d4923a6fdcd2a62c1ddf15
sandhydhannova <sandhydhannova01@gmail.com>
2023-12-11 07:48:41 +0700

    finalisasi

--- src/public/script/globals/config.js
+++ src/public/script/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   moneyFormatter: new Intl.NumberFormat(),
-  API: "http://localhost:3001"
+  API: "https://be-2-surabaya-15-n0r32en4f-sandhy-dhannovas-projects.vercel.app"
 };
 export default CONFIG;
